4cb85328bb7d914ac3850c73931118492a753029
Yash Ghugardare <yghugardare6@gmail.com>
2024-08-08 00:02:40 +0530

    fix: build error
    ' => &apos;

--- app/dashboard/interview/[mockId]/page.tsx
+++ app/dashboard/interview/[mockId]/page.tsx
@@ -40,7 +40,7 @@ function Interview({ params }: InterviewProp) {
   }
   return (
     <div className="my-10 ">
-      <h2 className="font-bold text-2xl">Let's Get Started</h2>
+      <h2 className="font-bold text-2xl">Let&apos;s Get Started</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
         <div className="flex flex-col my-5 gap-5 ">
           <div className="flex flex-col p-5 rounded-lg border gap-5">
