113c33e6c3b1215dc69abbb5272f27ad4e66021a
Yash Ghugardare <yghugardare6@gmail.com>
2024-08-07 13:48:16 +0530

    feat: user answer table added to postgre sql

--- utils/schema.ts
+++ utils/schema.ts
@@ -10,3 +10,15 @@ export const MockInterview = pgTable("mockInterview", {
   createdAt: varchar("createdAt"),
   mockId: varchar("mockId").notNull(),
 });
+
+export const UserAnswer = pgTable("UserAnswer", {
+  id: serial("id").primaryKey(),
+  mockIdRef: varchar("mockId").notNull(),
+  question: varchar("question").notNull(),
+  correctAns: text("correctAns"),
+  userAns: text("userAns"),
+  feedback: text("feedback"),
+  rating: varchar("rating"),
+  userEmail: varchar("userEmail"),
+  createdAt: varchar("createdAt"),
+});
